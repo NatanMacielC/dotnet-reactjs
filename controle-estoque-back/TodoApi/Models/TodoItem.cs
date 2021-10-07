ffef0681eced15216fc7510a7c5e11b201d4aeee
NatanMacielC <natan.macielc@gmail.com>
2021-10-07 14:17:26 -0300

    ajuste back

---
+++ controle-estoque-back/TodoApi/Models/TodoItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Preco { get; set; }
+        public string Fornecedor { get; set; }
+        public string Marca { get; set; }
+        public DateTime DataCadastro { get; set; }
+     
+    }
+}
